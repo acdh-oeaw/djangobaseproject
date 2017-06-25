232bc9ccae181e9315bb1416769d3f113014db8c
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-06-25 12:34:58 +0200

    added version info

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -6,6 +6,8 @@ PROJECT_METADATA = {
     'author': 'Peter Andorfer',
     'subtitle': 'A django project to bootstrap further web-app developments',
     'description': 'This django projects includes some generic applications (webpage, vocabs, places, bib)',
-    'github': 'https://github.com/acdh-oeaw/',
+    'github': 'https://github.com/acdh-oeaw/djangobaseproject',
     'purpose_de': 'Ziel von Django Base Project ist die Schaffung einer einheitlichen und generischen Grundlage für Web Applikationen.',
-    'purpose_en': 'The purpose of Django Base Project is to bootstrap web developments.'}
+    'purpose_en': 'The purpose of Django Base Project is to bootstrap web developments.',
+    'version': '1.1.0'
+}
