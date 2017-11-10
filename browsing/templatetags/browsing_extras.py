e56d7718389a6298b1255b33b38509c92d7f1c63
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-11-10 14:37:23 +0100

    added nav_bar template tag

--- browsing/templatetags/browsing_extras.py
+++ browsing/templatetags/browsing_extras.py
@@ -1,5 +1,36 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
 register = template.Library()
+
+
+@register.simple_tag
+def nav_menu(app=None):
+
+    """creates links to all class of the passed in application
+    for which get_listview_url() methods have been registered"""
+
+    if app:
+        models = ContentType.objects.filter(app_label=app)
+        result = []
+        for x in models:
+            modelname = x.name
+            modelname = modelname.replace(" ", "").lower()
+            try:
+                fetched_model = ContentType.objects.get(
+                    app_label=app, model=modelname).model_class()
+                item = {
+                    'name': modelname.title(),
+                }
+            except:
+                item = {
+                    'name': None
+                }
+            try:
+                item['link'] = fetched_model.get_listview_url()
+                result.append(item)
+            except AttributeError:
+                item['link'] = None
+        return result
 
 
 @register.inclusion_tag('browsing/tags/class_definition.html', takes_context=True)
