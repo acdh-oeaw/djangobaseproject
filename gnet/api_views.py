4feb821217653d34c9cb1023dda130688a00ad7b
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-01-07 17:46:08 +0100

    api endpoint added

--- gnet/api_views.py
+++ gnet/api_views.py
@@ -1,10 +1,56 @@
 import spacy
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, schema
 from pygermanet import load_germanet
 
 gn = load_germanet()
 nlp = spacy.load('de_core_news_sm')
+
+
+@api_view(['GET', 'POST'])
+@schema(None)
+def textparser(request):
+    """
+    get:
+    Send any text as value of a *longtext* paramter
+
+    post:
+    Processes any german text which is sent as value of a *longtext* param
+
+    """
+    enriched = {}
+    if request.method == 'POST':
+        longtext = request.POST.get('longtext')
+    else:
+        longtext = request.GET.get('longtext')
+    if longtext:
+        doc = nlp("{}".format(longtext))
+        sents = [x for x in doc.sents]
+        result = []
+        for x in sents:
+            chunk = {}
+            chunk['sent'] = "{}".format(x)
+            chunk['tokens'] = []
+            for y in x:
+                parts = {}
+                parts['text'] = y.text
+                parts['lemma'] = y.lemma_
+                parts['pos'] = y.pos_
+                parts['tag'] = y.tag_
+                parts['dep'] = y.dep_
+                parts['shape'] = y.shape_
+                parts['is_alpha'] = y.is_alpha
+                chunk['tokens'].append(parts)
+            result.append(chunk)
+        enriched['result'] = result
+        return Response(enriched)
+    return Response(
+        {
+            "Param-Name": "longtext",
+            "Param-Value": "any text you want",
+            "POST": "form-data or x-www-form-urlencoded"
+        }
+    )
 
 
 @api_view()
