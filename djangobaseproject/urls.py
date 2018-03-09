63d74420cbc3257816bffb1e6ccf9e70f6e89042
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-03-09 17:28:33 +0100

    added autocomplete views/urls for app entities

--- djangobaseproject/urls.py
+++ djangobaseproject/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^sparql/', include('sparql.urls', namespace='sparql')),
     url(r'^vocabs/', include('vocabs.urls', namespace='vocabs')),
     url(r'^vocabs-ac/', include('vocabs.dal_urls', namespace='vocabs-ac')),
+    url(r'^entities-ac/', include('entities.dal_urls', namespace='entities-ac')),
     url(r'^entities/', include('entities.urls', namespace='entities')),
     url(r'^bib/', include('bib.urls', namespace='bib')),
     url(r'^', include('webpage.urls', namespace='webpage')),
