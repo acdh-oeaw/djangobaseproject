42863bd48bc6569b43ad20b366c95175cdbe97f4
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-03-12 12:23:59 +0100

    minor

--- browsing/urls.py
+++ browsing/urls.py
@@ -5,13 +5,12 @@ app_name = 'browsing'
 
 urlpatterns = [
     url(r'places/$', views.PlaceListView.as_view(), name='browse_places'),
-    url(r'places/(?P<pk>[0-9]+)$', views.PlaceListView.as_view(), name='place_detail'),
     url(r'places-rdf/$', views.PlaceRDFView.as_view(), name='rdf_places'),
     url(r'altnames/$', views.AlternativeNameListView.as_view(), name='browse_altnames'),
-    url(r'^altnames/(?P<pk>[0-9]+)$', views.AlternativeNameListView.as_view(),
-        name='alternativename_detail'),
     url(r'persons/$', views.PersonListView.as_view(), name='browse_persons'),
     url(r'persons-rdf/$', views.PersonRDFView.as_view(), name='rdf_persons'),
     url(r'institutions/$', views.InstitutionListView.as_view(), name='browse_institutions'),
-    url(r'institutions-rdf/$', views.InstitutionRDFView.as_view(), name='rdf_institutions')
+    url(r'institutions-rdf/$', views.InstitutionRDFView.as_view(), name='rdf_institutions'),
+    url(r'repolocs/$', views.RepoLocationListView.as_view(), name='browse_repolocs'),
+    url(r'archres/$', views.ArchResourceListView.as_view(), name='browse_archres'),
 ]
