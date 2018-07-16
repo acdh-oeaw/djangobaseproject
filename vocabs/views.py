ebac794236490d9c207e97d786c53d5d52ebb4d9
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-07-16 14:54:51 +0200

    replaced webpage.utils with browsing.browsing_utils

--- vocabs/views.py
+++ vocabs/views.py
@@ -9,7 +9,7 @@ from .models import SkosConcept, SkosConceptScheme, SkosLabel
 from .forms import *
 from .tables import SkosConceptTable, SkosConceptSchemeTable, SkosLabelTable
 from .filters import SkosConceptListFilter, SkosConceptSchemeListFilter, SkosLabelListFilter
-from webpage.utils import GenericListView, BaseCreateView, BaseUpdateView
+from browsing.browsing_utils import GenericListView, BaseCreateView, BaseUpdateView
 
 
 class SkosConceptListView(GenericListView):
@@ -22,29 +22,6 @@ class SkosConceptListView(GenericListView):
         'pref_label',
         'broader_concept',
     ]
-
-    def get_all_cols(self):
-        all_cols = list(self.table_class.base_columns.keys())
-        return all_cols
-
-    def get_context_data(self, **kwargs):
-        context = super(SkosConceptListView, self).get_context_data()
-        context[self.context_filter_name] = self.filter
-        togglable_colums = [x for x in self.get_all_cols() if x not in self.init_columns]
-        context['togglable_colums'] = togglable_colums
-        return context
-
-    def get_table(self, **kwargs):
-        table = super(GenericListView, self).get_table()
-        RequestConfig(self.request, paginate={
-            'page': 1, 'per_page': self.paginate_by
-        }).configure(table)
-        default_cols = self.init_columns
-        all_cols = self.get_all_cols()
-        selected_cols = self.request.GET.getlist("columns") + default_cols
-        exclude_vals = [x for x in all_cols if x not in selected_cols]
-        table.exclude = exclude_vals
-        return table
 
 
 class SkosConceptDetailView(DetailView):
