2c7f776424d4766b721e0608b3ce78628ac1dc60
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-07-22 09:32:53 +0200

    added custom context processor

--- djangobaseproject/settings/base.py
+++ djangobaseproject/settings/base.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'webpage.webpage_content_processors.installed_apps',
             ],
         },
     },
