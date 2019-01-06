0ae31e7007c9eff0be565e6d17366adefdeb0ec1
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-01-06 10:02:37 +0100

    api endpoint for project's metadata added, requirements.txt updated

--- webpage/views.py
+++ webpage/views.py
@@ -1,10 +1,17 @@
+from copy import deepcopy
+
 from django.conf import settings
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login, logout
-from .forms import form_user_login
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from . forms import form_user_login
+from . metadata import PROJECT_METADATA as PM
 
 
 class GenericWebpageView(TemplateView):
@@ -51,3 +58,21 @@ def user_logout(request):
 
 def handler404(request, exception):
     return render(request, 'webpage/404-error.html', locals())
+
+
+@api_view()
+def project_info(request):
+
+    """
+    get:
+    returns a dict providing metadata about the current project
+    """
+
+    info_dict = deepcopy(PM)
+
+    if request.user.is_authenticated:
+        pass
+    else:
+        del info_dict['matomo_id']
+        del info_dict['matomo_url']
+    return Response(info_dict)
